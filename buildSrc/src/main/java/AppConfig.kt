1814475e4d115c1902bcf265bd84c1f9ac0f4cb7
Mikail Yusuf <mikailkyusuf.com>
2021-05-02 14:12:52 +0100

    Set Up Kotlin DSL

---
+++ buildSrc/src/main/java/AppConfig.kt
@@ -0,0 +1,128 @@
+ object buildConfigVersions {
+
+    val compileSdkVersion = 30
+    val buildToolsVersion = "30.0.2"
+    val minSdkVersion = 21
+    val targetSdkVersion = 30
+    val versionCode = 1
+    val versionName = "1.0"
+}
+
+
+object kotlinDependencies {
+
+    val legacySupportV4 =
+        "androidx.legacy:legacy-support-v4:${DependencyVersions.legacysupportv4Version}"
+    val lifecycleExtention =
+        "androidx.lifecycle:lifecycle-extensions:${DependencyVersions.lifecycleextentionVersion}"
+    val lifecycleViewmodel =
+        "androidx.lifecycle:lifecycle-viewmodel-ktx:${DependencyVersions.lifecycleViewmodel}"
+
+    val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${DependencyVersions.kotlin_version}"
+    val coroutines =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-android:${DependencyVersions.coroutinesAdroidVersion}"
+}
+
+
+object materialDesignDependencies {
+    val materialDesign =
+        "com.google.android.material:material:${DependencyVersions.materialDesignVersion}"
+}
+
+object coroutineDependencies {
+    val coroutineAndroid =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-android:${DependencyVersions.coroutineAndroid}"
+    val lifecycleLivedata =
+        "androidx.lifecycle:lifecycle-livedata-ktx:${DependencyVersions.lifecycleLivedata}"
+}
+
+
+object RetrofitDependencies {
+    val retrofit = "com.squareup.retrofit2:retrofit:2.9.0"
+    val retrofitRxJavaAdapter = "com.squareup.retrofit2:adapter-rxjava2:2.8.1"
+    val retrofitGsonConverter = "com.squareup.retrofit2:converter-gson:2.9.0"
+    val retrofitLoggingInterceptor = "com.squareup.okhttp3:logging-interceptor:4.9.0"
+
+}
+
+object MaterialSearchView {
+
+    val materialSearchView =
+        "com.miguelcatalan:materialsearchview:${DependencyVersions.materialSearchViewVersion}"
+}
+
+object rxBindingDependencies {
+
+    val rxbinding = "com.jakewharton.rxbinding3:${DependencyVersions.rxBinding}"
+
+    val rxBindingSupportv4 =
+        "com.jakewharton.rxbinding:rxbinding-support-v4:${DependencyVersions.rxbindingsupportV4}"
+
+    val rxJavaForRoom = "androidx.room:room-rxjava2:${DependencyVersions.rxJavaRoomVersion}"
+
+
+}
+
+
+object AndroidXDependencies {
+
+    val appCompat = "androidx.appcompat:appcompat:${DependencyVersions.appCompatVersion}"
+
+    val cardview = "androidx.cardview:cardview:${DependencyVersions.cardviewVersion}"
+    val viewpager2 =
+        "androidx.viewpager2:viewpager2:${DependencyVersions.viewpager2Version}"
+    val constraintLayout =
+        "androidx.constraintlayout:constraintlayout:${DependencyVersions.constraintLayoutVersion}"
+
+}
+
+object hiltDependencies {
+
+    val hiltAdroidImpl = "com.google.dagger:hilt-android:${DependencyVersions.hiltVersion}"
+    val hiltKaptCompiler =
+        "com.google.dagger:hilt-android-compiler:${DependencyVersions.hiltVersion}"
+    val hiltLifecycleViewmodel =
+        "androidx.hilt:hilt-lifecycle-viewmodel:${DependencyVersions.daggerHiltLifecycleViewmodelVersion}"
+    val hiltCompilerKapt =
+        "androidx.hilt:hilt-compiler:${DependencyVersions.daggerHiltLifecycleViewmodelVersion}"
+
+}
+
+
+object navigationDependencies {
+
+    val navigationFragmentKtx =
+        "androidx.navigation:navigation-fragment-ktx:${DependencyVersions.navVersion}"
+    val navigationUi = "androidx.navigation:navigation-ui-ktx:${DependencyVersions.navVersion}"
+    val fragment = "androidx.fragment:fragment-ktx:${DependencyVersions.fragmentVersion}"
+}
+
+
+object roomDependencies {
+
+    val runtime = "androidx.room:room-runtime:${DependencyVersions.androidRoomVersion}"
+    val kapt = "androidx.room:room-compiler:${DependencyVersions.androidRoomVersion}"
+    val impl = "androidx.room:room-ktx:${DependencyVersions.androidRoomVersion}"
+}
+
+object glideDepencies {
+
+    val glide = "com.github.bumptech.glide:glide:${DependencyVersions.glideVersion}"
+    val glideCompiler = "com.github.bumptech.glide:compiler:${DependencyVersions.glideVersion}"
+}
+
+object IndicatorDependency {
+    val indicator = "com.github.zhpanvip:viewpagerindicator:${DependencyVersions.indicatorVersion}"
+}
+
+object FireBaseDependency {
+
+    val firebaseCrashlytics =
+        "com.google.firebase:firebase-crashlytics-ktx:${DependencyVersions.firebaseCrashlyticsVersion}"
+    val firebaseAnalytics =
+        "com.google.firebase:firebase-analytics-ktx:${DependencyVersions.firebaseAnalyticsVersion}"
+}
+
+object TimerDependency {
+    val timber = "com.jakewharton.timber:timber:${DependencyVersions.timberVersion}"
+}
---
+++ buildSrc/src/main/java/DependencyVersions.kt
@@ -0,0 +1,52 @@
+object DependencyVersions {
+
+    val appCompatVersion = "1.2.0"
+    val kotlinVersion = "1.3.72"
+
+
+    val constraintLayoutVersion = "2.0.4"
+    val coroutinesAdroidVersion = "1.4.1"
+    val kotlin_version = "1.3.41"
+    val viewpager2Version = "1.0.0"
+    val multidexVersion = "2.3.0"
+    val fragmentVersion = "1.2.5"
+
+    val indicatorVersion = "1.2.0"
+
+    val firebaseFirestoreVersion = "22.0.2"
+    val firebaseAuthVersion = "22.0.2"
+    val coroutinesPlayServicesVersion = "1.1.1"
+
+    val glideVersion = "4.11.0"
+    val androidRoomVersion = "2.2.6"
+    val navVersion = "2.3.0"
+
+    val hiltVersion = "2.31.2-alpha"
+    val daggerHiltLifecycleViewmodelVersion = "1.0.0-alpha03"
+
+    val materialSearchViewVersion = "1.4.0"
+
+    val calenderviewVersion = "0.4.2"
+
+    val rxBinding = "rxbinding:3.1.0"
+    val rxbindingsupportV4 = "0.4.0"
+
+
+    val coroutineAndroid = "1.4.1"
+    val lifecycleLivedata = "2.3.0-rc01"
+
+    val materialDesignVersion = "1.2.1"
+    val cardviewVersion = "1.0.0"
+
+    val legacysupportv4Version = "1.0.0"
+    val lifecycleextentionVersion = "2.2.0"
+    val lifecycleViewmodel = "2.2.0"
+
+    const val firebaseCrashlyticsVersion = "17.4.0"
+    const val firebaseAnalyticsVersion = "18.0.2"
+
+    const val timberVersion = "4.7.1"
+
+    const val rxJavaRoomVersion = "2.2.2"
+
+}
